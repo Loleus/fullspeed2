56bd11d10f3dbb585f58273f4ba30d955c835206
Lukasz Kaminski <44691382+Loleus@users.noreply.github.com>
2025-07-01 10:41:58 +0200

    scene2

--- src/config/gameConfig.js
+++ src/config/gameConfig.js
@@ -5,11 +5,11 @@ export const CONFIG = {
   MASS: 1230, // masa auta w kg
   INV_MASS: 1 / 1230, // odwrotność masy (prekalkulowana dla wydajności)
   ACCEL: 0.8, // przyspieszenie
-  REVERSE_ACCEL: 0.5, // przyspieszenie na wstecznym
-  BRAKE: 0.08, // siła hamowania (klasyczna, stabilna)
+  REVERSE_ACCEL: 0.4, // przyspieszenie na wstecznym
+  BRAKE: 0.05, // siła hamowania (klasyczna, stabilna)
   FRICTION: 0.02, // tarcie
   FRICTION_MULT: 1 - 0.02, // mnożnik tarcia (prekalkulowany)
-  DRAG: 0.05, // opór powietrza
+  DRAG: 1, // opór powietrza
   GRIP: 1, // przyczepność
   WHEELBASE: 70, // rozstaw osi
   ENGINE_MULTIPLIER: 800, // mnożnik mocy silnika
@@ -17,7 +17,7 @@ export const CONFIG = {
   STEERING_SPEED: 0.005, // szybkość skręcania
   STEERING_RETURN: 0.085, // powrót kierownicy
   STOP_EPS: 0.001, // próg zatrzymania
-  WALL_BOUNCE: 0.30, // odbicie od ściany
-  THROTTLE_RAMP: 0.03, // narastanie gazu
+  WALL_BOUNCE: 0.35, // odbicie od ściany
+  THROTTLE_RAMP: 0.005, // narastanie gazu
   WORLD: { width: 6144, height: 6144} // rozmiar świata
 };
